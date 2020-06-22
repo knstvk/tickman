e9e0df45b8a1b81de946b4e37438d0cb0c75b31f
Konstantin Krivopustov <krivopustov@haulmont.com>
2020-06-22 11:40:06 +0400

    Init users; Update team

--- src/main/java/com/haulmont/tickman/TickmanApplication.java
+++ src/main/java/com/haulmont/tickman/TickmanApplication.java
@@ -1,5 +1,12 @@
 package com.haulmont.tickman;
 
+import io.jmix.core.security.UserRepository;
+import io.jmix.core.security.impl.CoreUser;
+import io.jmix.core.security.impl.InMemoryUserRepository;
+import io.jmix.security.role.assignment.RoleAssignment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -12,10 +19,18 @@ import javax.sql.DataSource;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-public class TickmanApplication {
+public class TickmanApplication implements ApplicationRunner {
+
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TickmanApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		initUsers();
 	}
 
 	@Bean
@@ -23,5 +38,12 @@ public class TickmanApplication {
 	@ConfigurationProperties(prefix="main.datasource")
 	DataSource dataSource() {
 		return DataSourceBuilder.create().build();
+	}
+
+	private void initUsers() {
+		CoreUser admin = new CoreUser("admin", "{noop}admin", "Admin");
+		if (userRepository instanceof InMemoryUserRepository) {
+			((InMemoryUserRepository) userRepository).createUser(admin);
+		}
 	}
 }
